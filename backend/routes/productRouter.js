1aac99cb0aa807551409aa21356a79e010493f6c
arthur-asev <arthurginho02@gmail.com>
2022-08-16 18:59:58 -0300

    adiconado componente barra de pesquisa e criado categorias de produtos

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import expressAsyncHandler from 'express-async-handler';
+
 
 const productRouter = express.Router();
 
@@ -16,6 +18,15 @@ productRouter.get('/slug/:slug', async (req, res) => {
         res.status(404).send({ message: 'Produto não encontrado' });
     }
 });
+
+productRouter.get(
+    '/categories',
+    expressAsyncHandler(async (req, res) => {
+      const categories = await Product.find().distinct('category');
+      res.send(categories);
+    })
+  );
+  
 productRouter.get('/:id', async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import { useContext } from 'react';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/esm/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import SearchBox from './components/SearchBox';
 import CartPage from './pages/Cart/CartPage';
 import HomePage from './pages/HomePage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
@@ -20,6 +23,7 @@ import ShippingAddressPage from './pages/ShippingAddressPage';
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/Signup/SignupPage';
 import { Store } from './Store';
+import { getError } from './utils';
 
 
 
@@ -37,18 +41,47 @@ function App() {
     window.location.href = '/signin';
   };
 
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div
+        className={
+          sidebarIsOpen
+            ? 'd-flex flex-column site-container active-cont'
+            : 'd-flex flex-column site-container'
+        }
+      >
         <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar className='nav' bg="dark" variant="dark" expand="lg" >
             <Container>
+              <Button
+                variant="dark"
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className="fas fa-bars"></i>
+              </Button>
+
               <LinkContainer to="/">
                 <Navbar.Brand><img width={200} src='/images/logo.svg' alt='logo' /></Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBox/>
                 <Nav className="me-auto  w-100  justify-content-end">
                   <Link to="/cart" className="nav-link">
                     Carrinho
@@ -85,6 +118,29 @@ function App() {
             </Container>
           </Navbar>
         </header>
+        <div
+          className={
+            sidebarIsOpen
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categorias</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={`/search?category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
         <main>
           <Container className="mt-3">
             <Routes>
